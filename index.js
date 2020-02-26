64d55c379485352ecbab15c95a2dbda9e6a7c74f
[stefanhallberg123] <[stefanhalllberg@live.se]>
2020-02-26 13:58:09 +0100

    gjorde om lite

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Härifrån startar vi upp vår webshop
 const mongoose = require("mongoose");
-const dbUrl =
-  process.env.MONGO_ATLAS_URL || require("./config/config").databaseURL; // the right way!
+const dbUrl = process.env.MONGO_ATLAS_URL;
 const { app, port } = require("./src/server");
 
 // Kicka igång servern
